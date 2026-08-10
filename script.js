3cc4ffac6955424e8e1408ecd899f50aa764caad
Lomos <Adrien_ca@hotmail.com>
2026-08-10 17:44:34 -0400

    Update map initialization to use maxZoom option

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   // 1. Initialize map on Vaughan
-  const map = L.map('map',{setZoom:19}).setView([43.8372, -79.5083], 13);
+  const map = L.map('map', { maxZoom: 19 }).setView([43.8372, -79.5083], 13);
 
   // 2. OpenStreetMap backup basemap
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
