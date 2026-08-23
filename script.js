f4d601136b81e7577aba50831f7da643ab89bba2
Lomos <Adrien_ca@hotmail.com>
2026-08-23 17:39:19 -0400

    Increase maxZoom for OpenStreetMap layer to 24

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // 2. OpenStreetMap backup basemap
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 22,
+    maxZoom: 24,
     attribution: '© OpenStreetMap',
     zIndex: 1
   }).addTo(map);
